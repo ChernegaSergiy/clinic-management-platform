161a55cff4211684b877f7cb86889313273e7b05
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-21 11:35:12 +0200

    feat(appointments): Add create and store methods to AppointmentController

--- src/Controller/AppointmentController.php
+++ src/Controller/AppointmentController.php
@@ -4,14 +4,20 @@ namespace App\Controller;
 
 use App\Core\View;
 use App\Repository\AppointmentRepository;
+use App\Repository\PatientRepository;
+use App\Repository\UserRepository;
 
 class AppointmentController
 {
     private AppointmentRepository $appointmentRepository;
+    private PatientRepository $patientRepository;
+    private UserRepository $userRepository;
 
     public function __construct()
     {
         $this->appointmentRepository = new AppointmentRepository();
+        $this->patientRepository = new PatientRepository();
+        $this->userRepository = new UserRepository();
     }
 
     public function index(): void
@@ -22,5 +28,44 @@ class AppointmentController
         }
         $appointments = $this->appointmentRepository->findAll();
         View::render('appointments/index.html.twig', ['appointments' => $appointments]);
+    }
+
+    public function create(): void
+    {
+        if (!isset($_SESSION['user'])) {
+            header('Location: /login');
+            exit();
+        }
+
+        $patients = $this->patientRepository->findAllActive();
+        $doctors = $this->userRepository->findAllDoctors();
+
+        $patientOptions = [];
+        foreach ($patients as $patient) {
+            $patientOptions[$patient['id']] = $patient['full_name'];
+        }
+
+        $doctorOptions = [];
+        foreach ($doctors as $doctor) {
+            $doctorOptions[$doctor['id']] = $doctor['full_name'];
+        }
+
+        View::render('appointments/new.html.twig', [
+            'patients' => $patientOptions,
+            'doctors' => $doctorOptions,
+        ]);
+    }
+
+    public function store(): void
+    {
+        if (!isset($_SESSION['user'])) {
+            header('Location: /login');
+            exit();
+        }
+
+        // TODO: Add validation
+        $this->appointmentRepository->save($_POST);
+        header('Location: /appointments');
+        exit();
     }
 }
