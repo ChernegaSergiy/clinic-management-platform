f241651be827c799cd1a85dd1b97e06714bf2f10
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-21 12:24:08 +0200

    feat(admin): Add create and store methods to AdminController

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,14 +4,17 @@ namespace App\Controller;
 
 use App\Core\View;
 use App\Repository\UserRepository;
+use App\Repository\RoleRepository;
 
 class AdminController
 {
     private UserRepository $userRepository;
+    private RoleRepository $roleRepository;
 
     public function __construct()
     {
         $this->userRepository = new UserRepository();
+        $this->roleRepository = new RoleRepository();
     }
 
     public function users(): void
@@ -22,5 +25,34 @@ class AdminController
         }
         $users = $this->userRepository->findAll();
         View::render('admin/users.html.twig', ['users' => $users]);
+    }
+
+    public function createUser(): void
+    {
+        if (!isset($_SESSION['user']) || $_SESSION['user']['role_id'] !== 1) { // Assuming role_id 1 is admin
+            header('Location: /login');
+            exit();
+        }
+
+        $roles = $this->roleRepository->findAll();
+        $roleOptions = [];
+        foreach ($roles as $role) {
+            $roleOptions[$role['id']] = $role['name'];
+        }
+
+        View::render('admin/new_user.html.twig', ['roles' => $roleOptions]);
+    }
+
+    public function storeUser(): void
+    {
+        if (!isset($_SESSION['user']) || $_SESSION['user']['role_id'] !== 1) { // Assuming role_id 1 is admin
+            header('Location: /login');
+            exit();
+        }
+
+        // TODO: Add validation
+        $this->userRepository->save($_POST);
+        header('Location: /admin/users');
+        exit();
     }
 }
