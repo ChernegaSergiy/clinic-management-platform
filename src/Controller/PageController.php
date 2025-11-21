cad8ab4daf4e862501d51e0748cf1d12e5cf5422
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-21 11:23:07 +0200

    refactor(core): Add PageController and View class

---
+++ src/Controller/PageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Core\View;
+use Symfony\Component\Yaml\Yaml;
+
+class PageController
+{
+    public function home(): void
+    {
+        $content = Yaml::parseFile(__DIR__ . '/../../content/home.uk.yml');
+        View::render('home/index.html.twig', ['page' => $content]);
+    }
+
+    public function about(): void
+    {
+        View::render('about/index.html.twig');
+    }
+
+    public function contact(): void
+    {
+        View::render('contact/index.html.twig');
+    }
+}
---
+++ src/Core/View.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Core;
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+class View
+{
+    private static ?Environment $twig = null;
+
+    private static function getTwig(): Environment
+    {
+        if (self::$twig === null) {
+            $loader = new FilesystemLoader(__DIR__ . '/../../templates');
+            self::$twig = new Environment($loader, [
+                // 'cache' => __DIR__ . '/../../var/cache',
+            ]);
+            self::$twig->addGlobal('session', $_SESSION);
+        }
+        return self::$twig;
+    }
+
+    public static function render(string $template, array $data = []): void
+    {
+        echo self::getTwig()->render($template, $data);
+    }
+}
