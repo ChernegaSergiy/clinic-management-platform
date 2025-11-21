f7e4c87ad0c96c6107f8f8ad89944e8e7645420b
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-21 12:03:11 +0200

    feat(inventory): Add create and store methods to InventoryController

--- src/Controller/InventoryController.php
+++ src/Controller/InventoryController.php
@@ -23,4 +23,26 @@ class InventoryController
         $items = $this->inventoryItemRepository->findAll();
         View::render('inventory/index.html.twig', ['items' => $items]);
     }
+
+    public function create(): void
+    {
+        if (!isset($_SESSION['user'])) {
+            header('Location: /login');
+            exit();
+        }
+        View::render('inventory/new.html.twig');
+    }
+
+    public function store(): void
+    {
+        if (!isset($_SESSION['user'])) {
+            header('Location: /login');
+            exit();
+        }
+
+        // TODO: Add validation
+        $this->inventoryItemRepository->save($_POST);
+        header('Location: /inventory');
+        exit();
+    }
 }
