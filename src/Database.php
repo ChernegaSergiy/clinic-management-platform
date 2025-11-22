55e3f0ea82eda36cc9ad9625efe61378aacd587f
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-22 17:38:59 +0200

    refactor(db): Remove SQLite DSN branch

--- src/Database.php
+++ src/Database.php
@@ -20,13 +20,7 @@ class Database
             $dbUser = $_ENV['DB_USERNAME'] ?? 'root';
             $dbPass = $_ENV['DB_PASSWORD'] ?? '';
             $dbPort = $_ENV['DB_PORT'] ?? '3306';
-            $dbConnection = $_ENV['DB_CONNECTION'] ?? 'mysql';
-
-            if ($dbConnection === 'sqlite') {
-                $dsn = "sqlite:" . __DIR__ . "/../database/" . $dbName; // Assuming $dbName is the file name, e.g., clinic.sqlite
-            } else {
-                $dsn = "mysql:host={$dbHost};port={$dbPort};dbname={$dbName};charset=utf8mb4";
-            }
+            $dsn = "mysql:host={$dbHost};port={$dbPort};dbname={$dbName};charset=utf8mb4";
 
             try {
                 self::$instance = new PDO($dsn, $dbUser, $dbPass, [
