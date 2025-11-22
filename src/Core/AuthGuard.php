21f1a53831e2979b61722484a0448f662bf631cd
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-22 15:44:49 +0200

    feat(core): Add AuthGuard service

---
+++ src/Core/AuthGuard.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Core;
+
+class AuthGuard
+{
+    public static function check(): void
+    {
+        if (!isset($_SESSION['user'])) {
+            header('Location: /login');
+            exit();
+        }
+    }
+
+    public static function isAdmin(): void
+    {
+        self::check();
+        if ($_SESSION['user']['role_id'] !== 1) {
+            http_response_code(403);
+            echo "Доступ заборонено";
+            exit();
+        }
+    }
+}
