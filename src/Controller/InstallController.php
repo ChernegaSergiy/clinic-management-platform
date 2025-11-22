933b674bee0762c950e0a418af03dfb397c8f3ba
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-22 17:24:19 +0200

    fix(install): Relax ENUM conversion for SQLite

--- src/Controller/InstallController.php
+++ src/Controller/InstallController.php
@@ -188,7 +188,7 @@ class InstallController
             '/DEFAULT\\s+CURRENT_TIMESTAMP\\s+ON\\s+UPDATE\\s+CURRENT_TIMESTAMP/i' => 'DEFAULT CURRENT_TIMESTAMP',
             '/ON\\s+UPDATE\\s+CURRENT_TIMESTAMP/i' => '',
             '/ENGINE=InnoDB.*;/i' => ';',
-            '/ENUM\\(([^)]*)\\)/i' => 'TEXT CHECK (VALUE IN (\1))',
+            '/ENUM\\(([^)]*)\\)/i' => 'TEXT',
             '/\\bUNSIGNED\\b/i' => '',
         ];
 
