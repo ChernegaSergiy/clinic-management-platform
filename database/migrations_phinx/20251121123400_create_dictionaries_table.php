8ec67fa721bb9e0eef39f9f029d45e4ef8d9ece7
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-24 19:18:46 +0200

    fix: Correct chaining syntax in CreateDictionariesTable migration

--- database/migrations_phinx/20251121123400_create_dictionaries_table.php
+++ database/migrations_phinx/20251121123400_create_dictionaries_table.php
@@ -6,7 +6,7 @@ class CreateDictionariesTable extends AbstractMigration
     {
         $table = $this->table('dictionaries', ['id' => false, 'primary_key' => 'id']);
         $table->addColumn('id', 'integer', ['identity' => true, 'signed' => false])
-        $table->addColumn('name', 'string', ['limit' => 255])
+              ->addColumn('name', 'string', ['limit' => 255])
               ->addColumn('description', 'text', ['null' => true])
               ->addColumn('type', 'string', ['limit' => 255, 'null' => true])
               ->addTimestamps()
