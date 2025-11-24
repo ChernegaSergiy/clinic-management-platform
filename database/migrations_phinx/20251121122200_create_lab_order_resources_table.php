d8bae22dd6b1328d02769bcb227894679e616d38
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-24 18:51:26 +0200

    fix: Ensure unsigned integers for foreign key relationships in lab_order_resources migration

--- database/migrations_phinx/20251121122200_create_lab_order_resources_table.php
+++ database/migrations_phinx/20251121122200_create_lab_order_resources_table.php
@@ -5,10 +5,12 @@ class CreateLabOrderResourcesTable extends AbstractMigration
     public function change()
     {
         $table = $this->table('lab_order_resources', ['id' => false, 'primary_key' => ['lab_order_id', 'lab_resource_id']]);
-        $table->addColumn('lab_order_id', 'integer')
-              ->addColumn('lab_resource_id', 'integer')
+        $table->addColumn('lab_order_id', 'integer', ['signed' => false])
+              ->addColumn('lab_resource_id', 'integer', ['signed' => false])
               ->addForeignKey('lab_order_id', 'lab_orders', 'id', ['delete'=> 'CASCADE', 'update'=> 'NO_ACTION'])
               ->addForeignKey('lab_resource_id', 'lab_resources', 'id', ['delete'=> 'CASCADE', 'update'=> 'NO_ACTION'])
+              ->addIndex(['lab_order_id'])
+              ->addIndex(['lab_resource_id'])
               ->create();
     }
 }
