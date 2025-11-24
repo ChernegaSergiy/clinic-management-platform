36bbb9a9af0c14a3e76ae727d8ec5ce412d2b7af
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-24 19:01:57 +0200

    fix: Ensure unsigned integers for foreign key relationships in bundle_services migration

--- database/migrations_phinx/20251121122900_create_bundle_services_table.php
+++ database/migrations_phinx/20251121122900_create_bundle_services_table.php
@@ -5,10 +5,12 @@ class CreateBundleServicesTable extends AbstractMigration
     public function change()
     {
         $table = $this->table('bundle_services', ['id' => false, 'primary_key' => ['bundle_id', 'service_id']]);
-        $table->addColumn('bundle_id', 'integer')
-              ->addColumn('service_id', 'integer')
+        $table->addColumn('bundle_id', 'integer', ['signed' => false])
+              ->addColumn('service_id', 'integer', ['signed' => false])
               ->addForeignKey('bundle_id', 'service_bundles', 'id', ['delete'=> 'CASCADE', 'update'=> 'NO_ACTION'])
               ->addForeignKey('service_id', 'services', 'id', ['delete'=> 'CASCADE', 'update'=> 'NO_ACTION'])
+              ->addIndex(['bundle_id'])
+              ->addIndex(['service_id'])
               ->create();
     }
 }
