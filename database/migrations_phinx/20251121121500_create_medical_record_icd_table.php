7817f43d220377638531f0039af8f8baab952ba3
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-24 18:37:59 +0200

    fix: Ensure unsigned integers for foreign key relationships in medical_record_icd migration

--- database/migrations_phinx/20251121121500_create_medical_record_icd_table.php
+++ database/migrations_phinx/20251121121500_create_medical_record_icd_table.php
@@ -5,10 +5,12 @@ class CreateMedicalRecordIcdTable extends AbstractMigration
     public function change()
     {
         $table = $this->table('medical_record_icd', ['id' => false, 'primary_key' => ['medical_record_id', 'icd_code_id']]);
-        $table->addColumn('medical_record_id', 'integer')
-              ->addColumn('icd_code_id', 'integer')
+        $table->addColumn('medical_record_id', 'integer', ['signed' => false])
+              ->addColumn('icd_code_id', 'integer', ['signed' => false])
               ->addForeignKey('medical_record_id', 'medical_records', 'id', ['delete'=> 'CASCADE', 'update'=> 'NO_ACTION'])
               ->addForeignKey('icd_code_id', 'icd_codes', 'id', ['delete'=> 'CASCADE', 'update'=> 'NO_ACTION'])
+              ->addIndex(['medical_record_id'])
+              ->addIndex(['icd_code_id'])
               ->create();
     }
 }
