c7ea1e0e50ce3e68aaab886d44cef7e569e451b7
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-24 18:16:26 +0200

    fix: Correct chaining syntax in CreateRolesTable migration

--- database/migrations_phinx/20251121120000_create_roles_table.php
+++ database/migrations_phinx/20251121120000_create_roles_table.php
@@ -8,7 +8,7 @@ class CreateRolesTable extends AbstractMigration
     {
         $table = $this->table('roles', ['id' => false, 'primary_key' => 'id']);
         $table->addColumn('id', 'integer', ['identity' => true, 'signed' => false])
-        $table->addColumn('name', 'string', ['limit' => 255])
+              ->addColumn('name', 'string', ['limit' => 255])
               ->addColumn('description', 'text', ['null' => true])
               ->addTimestamps()
               ->addIndex(['name'], ['unique' => true])
