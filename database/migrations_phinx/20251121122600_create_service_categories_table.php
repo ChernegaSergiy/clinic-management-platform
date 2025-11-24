4f38627c89687fd2f92d4040a8c8ce052a61b8dd
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-24 18:59:08 +0200

    fix: Correct chaining syntax in CreateServiceCategoriesTable migration

--- database/migrations_phinx/20251121122600_create_service_categories_table.php
+++ database/migrations_phinx/20251121122600_create_service_categories_table.php
@@ -6,7 +6,7 @@ class CreateServiceCategoriesTable extends AbstractMigration
     {
         $table = $this->table('service_categories', ['id' => false, 'primary_key' => 'id']);
         $table->addColumn('id', 'integer', ['identity' => true, 'signed' => false])
-        $table->addColumn('name', 'string', ['limit' => 255])
+              ->addColumn('name', 'string', ['limit' => 255])
               ->addColumn('description', 'text', ['null' => true])
               ->addIndex(['name'], ['unique' => true])
               ->create();
