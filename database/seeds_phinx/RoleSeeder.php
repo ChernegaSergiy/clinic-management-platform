57b56901214b98cff80ea8641ebf5e73d34b1699
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-27 07:34:16 +0200

    chore: Seed extended roles for MIS RBAC

--- database/seeds_phinx/RoleSeeder.php
+++ database/seeds_phinx/RoleSeeder.php
@@ -12,12 +12,32 @@ class RoleSeeder extends AbstractSeed
                 'description' => 'Адміністратор системи'
             ],
             [
+                'name' => 'medical_manager',
+                'description' => 'Медичний керівник / методолог'
+            ],
+            [
                 'name' => 'doctor',
                 'description' => 'Лікар'
             ],
             [
                 'name' => 'registrar',
                 'description' => 'Реєстратор'
+            ],
+            [
+                'name' => 'nurse',
+                'description' => 'Медсестра / асистент'
+            ],
+            [
+                'name' => 'lab_technician',
+                'description' => 'Лаборант'
+            ],
+            [
+                'name' => 'billing',
+                'description' => 'Білінг / бухгалтерія'
+            ],
+            [
+                'name' => 'inventory_manager',
+                'description' => 'Комірник / менеджер складу'
             ]
         ];
 
