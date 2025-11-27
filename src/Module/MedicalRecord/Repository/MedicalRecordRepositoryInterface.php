89db9744930d4f291829f76221ad7427fe506756
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-27 08:10:44 +0200

    feat(medical): Add doctor filter to repository interface

--- src/Module/MedicalRecord/Repository/MedicalRecordRepositoryInterface.php
+++ src/Module/MedicalRecord/Repository/MedicalRecordRepositoryInterface.php
@@ -5,6 +5,7 @@ namespace App\Module\MedicalRecord\Repository;
 interface MedicalRecordRepositoryInterface
 {
     public function findByPatientId(int $patientId): array;
+    public function findByDoctorId(int $doctorId): array;
     public function findAll(): array;
     public function save(array $data): int|false;
     public function findById(int $id): ?array;
