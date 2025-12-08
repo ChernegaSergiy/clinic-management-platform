554df4f0e616dde54bd3febbf44b1e35a069a969
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-12-08 15:09:52 +0200

    feat(news): Create news_articles table migration

---
+++ database/migrations_phinx/20251208130628_create_news_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class CreateNewsArticlesTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * For more information on using tables please check:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#working-with-tables
+     */
+    public function change(): void
+    {
+        $table = $this->table('news_articles');
+        $table->addColumn('title', 'string', ['limit' => 255])
+              ->addColumn('meta', 'string', ['limit' => 500, 'comment' => 'Short description or summary'])
+              ->addColumn('content', 'text', ['comment' => 'Full article HTML content'])
+              ->addColumn('published_at', 'datetime', ['default' => 'CURRENT_TIMESTAMP'])
+              ->addColumn('author_id', 'integer', ['null' => true, 'comment' => 'Foreign key to users table'])
+              ->addForeignKey('author_id', 'users', 'id', ['delete' => 'SET_NULL', 'update' => 'CASCADE'])
+              ->addColumn('is_published', 'boolean', ['default' => true])
+              ->addTimestamps() // Adds created_at and updated_at
+              ->create();
+    }
+}
