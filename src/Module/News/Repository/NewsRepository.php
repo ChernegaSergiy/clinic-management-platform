b074f6e320642d53be36d010cbf81b8a90d09ee8
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-12-08 15:45:39 +0200

    fix(news): Correct PDO instantiation in NewsRepository

--- src/Module/News/Repository/NewsRepository.php
+++ src/Module/News/Repository/NewsRepository.php
@@ -11,7 +11,7 @@ class NewsRepository
 
     public function __construct()
     {
-        $this->pdo = Database::getInstance()->getConnection();
+        $this->pdo = Database::getInstance();
     }
 
     public function findAll(): array
