bc6ab6dd70ef535c337cd54f72cf5e964ab79ef4
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-04 01:47:55 +0200

    refactor: Remove dashboard prefix from Hrm routes

--- src/Module/Hrm/HrmModule.php
+++ src/Module/Hrm/HrmModule.php
@@ -10,12 +10,12 @@ class HrmModule extends BaseModule
 {
     public function registerRoutes(Router $router): void
     {
-        $router->add('GET', '/dashboard/hrm', [HrmController::class, 'index']);
-        $router->add('GET', '/dashboard/hrm/new', [HrmController::class, 'create']);
-        $router->add('POST', '/dashboard/hrm/new', [HrmController::class, 'store']);
-        $router->add('GET', '/dashboard/hrm/show', [HrmController::class, 'show']);
-        $router->add('GET', '/dashboard/hrm/edit', [HrmController::class, 'edit']);
-        $router->add('POST', '/dashboard/hrm/edit', [HrmController::class, 'update']);
-        $router->add('POST', '/dashboard/hrm/toggle-status', [HrmController::class, 'toggleStatus']);
+        $router->add('GET', '/hrm', [HrmController::class, 'index']);
+        $router->add('GET', '/hrm/new', [HrmController::class, 'create']);
+        $router->add('POST', '/hrm/new', [HrmController::class, 'store']);
+        $router->add('GET', '/hrm/show', [HrmController::class, 'show']);
+        $router->add('GET', '/hrm/edit', [HrmController::class, 'edit']);
+        $router->add('POST', '/hrm/edit', [HrmController::class, 'update']);
+        $router->add('POST', '/hrm/toggle-status', [HrmController::class, 'toggleStatus']);
     }
 }
