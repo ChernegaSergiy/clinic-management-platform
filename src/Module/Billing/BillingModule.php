471f592a2c61b89d0d5f13cd4589465f916ff09f
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-04 01:36:57 +0200

    feat: Add BillingModule with route registration

---
+++ src/Module/Billing/BillingModule.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Module\Billing;
+
+use App\Core\BaseModule;
+use App\Core\Router;
+use App\Module\Billing\BillingController;
+use App\Module\Billing\ContractController;
+
+class BillingModule extends BaseModule
+{
+    public function registerRoutes(Router $router): void
+    {
+        $router->add('GET', '/billing', [BillingController::class, 'index']);
+        $router->add('GET', '/billing/new', [BillingController::class, 'create']);
+        $router->add('POST', '/billing/new', [BillingController::class, 'store']);
+        $router->add('GET', '/billing/show', [BillingController::class, 'show']);
+        $router->add('GET', '/billing/edit', [BillingController::class, 'edit']);
+        $router->add('POST', '/billing/edit', [BillingController::class, 'update']);
+        $router->add('POST', '/billing/add-payment', [BillingController::class, 'addPayment']);
+        $router->add('GET', '/billing/export-pdf', [BillingController::class, 'exportInvoicesToPdf']);
+        $router->add('GET', '/billing/export-excel', [BillingController::class, 'exportInvoicesToExcel']);
+        $router->add('GET', '/billing/export-csv', [BillingController::class, 'exportInvoicesToCsv']);
+
+        $router->add('GET', '/billing/contracts', [ContractController::class, 'index']);
+        $router->add('GET', '/billing/contracts/new', [ContractController::class, 'create']);
+        $router->add('POST', '/billing/contracts/new', [ContractController::class, 'store']);
+        $router->add('GET', '/billing/contracts/show', [ContractController::class, 'show']);
+        $router->add('GET', '/billing/contracts/edit', [ContractController::class, 'edit']);
+        $router->add('POST', '/billing/contracts/edit', [ContractController::class, 'update']);
+        $router->add('POST', '/billing/contracts/delete', [ContractController::class, 'delete']);
+        $router->add('GET', '/billing/contracts/{id}/download', [ContractController::class, 'downloadFile']);
+    }
+}
