2606674b9f1ca908ccca8180b9d56c600b09c295
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-04 01:35:25 +0200

    feat: Add BaseModule abstract class with manifest support

---
+++ src/Core/BaseModule.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Core;
+
+abstract class BaseModule implements ModuleInterface
+{
+    protected string $manifestPath;
+    protected ?array $manifest = null;
+    protected string $basePath;
+
+    public function __construct()
+    {
+        $this->basePath = $this->getBasePath();
+        $this->manifestPath = $this->basePath . '/module.yaml';
+    }
+
+    public function getName(): string
+    {
+        return $this->getManifest()['name'] ?? $this->getDefaultName();
+    }
+
+    public function getVersion(): string
+    {
+        return $this->getManifest()['version'] ?? '1.0.0';
+    }
+
+    public function bootstrap(): void
+    {
+    }
+
+    public function registerRoutes(Router $router): void
+    {
+    }
+
+    protected function getManifest(): array
+    {
+        if ($this->manifest === null) {
+            if (file_exists($this->manifestPath)) {
+                $yaml = file_get_contents($this->manifestPath);
+                $this->manifest = \Symfony\Component\Yaml\Yaml::parse($yaml);
+            } else {
+                $this->manifest = [];
+            }
+        }
+
+        return $this->manifest;
+    }
+
+    protected function getDefaultName(): string
+    {
+        $className = (new \ReflectionClass($this))->getShortName();
+        return str_replace('Module', '', $className);
+    }
+
+    protected function getBasePath(): string
+    {
+        $reflector = new \ReflectionClass($this);
+        return dirname($reflector->getFileName());
+    }
+
+    protected function getConfig(string $key, $default = null)
+    {
+        return $this->getManifest()['config'][$key] ?? $default;
+    }
+}
