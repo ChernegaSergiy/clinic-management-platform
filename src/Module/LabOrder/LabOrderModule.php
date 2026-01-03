9665db6119de586c8459ffbbec42a0297d1fb7cb
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-04 01:37:33 +0200

    feat: Add LabOrderModule with route registration

---
+++ src/Module/LabOrder/LabOrderModule.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Module\LabOrder;
+
+use App\Core\BaseModule;
+use App\Core\Router;
+use App\Module\LabOrder\LabOrderController;
+
+class LabOrderModule extends BaseModule
+{
+    public function registerRoutes(Router $router): void
+    {
+        $router->add('GET', '/lab-orders/new', [LabOrderController::class, 'create']);
+        $router->add('POST', '/lab-orders/new', [LabOrderController::class, 'store']);
+        $router->add('GET', '/lab-orders/show', [LabOrderController::class, 'show']);
+        $router->add('GET', '/lab-orders/edit', [LabOrderController::class, 'edit']);
+        $router->add('POST', '/lab-orders/edit', [LabOrderController::class, 'update']);
+    }
+}
