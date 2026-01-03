53e986b2b97b3b57aae0d4161e7a14a419bcc466
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-04 01:38:14 +0200

    feat: Add InsuranceModule with route registration

---
+++ src/Module/Insurance/InsuranceModule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Module\Insurance;
+
+use App\Core\BaseModule;
+use App\Core\Router;
+use App\Module\Insurance\InsuranceController;
+
+class InsuranceModule extends BaseModule
+{
+    public function registerRoutes(Router $router): void
+    {
+        $router->add('GET', '/insurance/companies', [InsuranceController::class, 'index']);
+        $router->add('GET', '/insurance/companies/show', [InsuranceController::class, 'show']);
+        $router->add('GET', '/insurance/companies/new', [InsuranceController::class, 'create']);
+        $router->add('POST', '/insurance/companies/new', [InsuranceController::class, 'store']);
+        $router->add('GET', '/insurance/companies/edit', [InsuranceController::class, 'edit']);
+        $router->add('POST', '/insurance/companies/edit', [InsuranceController::class, 'update']);
+        $router->add('POST', '/insurance/companies/delete', [InsuranceController::class, 'delete']);
+        $router->add('GET', '/insurance/claims', [InsuranceController::class, 'listClaims']);
+        $router->add('GET', '/insurance/claims/show', [InsuranceController::class, 'showClaim']);
+        $router->add('POST', '/insurance/claims/update-status', [InsuranceController::class, 'updateClaimStatus']);
+    }
+}
