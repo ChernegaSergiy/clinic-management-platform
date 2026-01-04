290a79808cc05a121d0cb329ed2f77d9e5bf31ed
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-04 07:49:39 +0200

    feat: Add permissions and policies registration to LabOrderModule

--- src/Module/LabOrder/LabOrderModule.php
+++ src/Module/LabOrder/LabOrderModule.php
@@ -4,6 +4,8 @@ namespace App\Module\LabOrder;
 
 use App\Core\BaseModule;
 use App\Core\Router;
+use App\Core\PermissionRegistry;
+use App\Core\PolicyRegistry;
 use App\Module\LabOrder\LabOrderController;
 
 class LabOrderModule extends BaseModule
@@ -15,5 +17,21 @@ class LabOrderModule extends BaseModule
         $router->add('GET', '/lab-orders/show', [LabOrderController::class, 'show']);
         $router->add('GET', '/lab-orders/edit', [LabOrderController::class, 'edit']);
         $router->add('POST', '/lab-orders/edit', [LabOrderController::class, 'update']);
+    }
+
+    public function registerPermissions(PermissionRegistry $registry): void
+    {
+        $registry->add('lab.read', 'Перегляд лабораторних досліджень');
+        $registry->add('lab.write', 'Редагування лабораторних досліджень');
+
+        $registry->addRoleMapping('admin', ['lab.read', 'lab.write']);
+        $registry->addRoleMapping('medical_manager', ['lab.read']);
+        $registry->addRoleMapping('doctor', ['lab.read', 'lab.write']);
+        $registry->addRoleMapping('nurse', ['lab.read', 'lab.write']);
+        $registry->addRoleMapping('lab_technician', ['lab.read', 'lab.write']);
+    }
+
+    public function registerPolicies(PolicyRegistry $registry): void
+    {
     }
 }
