be23992f37b1e857add9ace3259b88144bc1f72f
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-04 09:02:28 +0200

    feat: Add PolicyRegistry registration to LabOrderModule

--- src/Module/LabOrder/LabOrderModule.php
+++ src/Module/LabOrder/LabOrderModule.php
@@ -19,17 +19,11 @@ class LabOrderModule extends BaseModule
         $router->add('POST', '/lab-orders/edit', [LabOrderController::class, 'update']);
     }
 
-    public function registerPermissions(PermissionRegistry $registry): void
+    public function registerPolicies(PolicyRegistry $registry): void
     {
-        $registry->add('lab.read', 'Перегляд лабораторних досліджень');
-        $registry->add('lab.write', 'Редагування лабораторних досліджень');
-
-        $registry->addRoleMapping('admin', ['lab.read', 'lab.write']);
-        $registry->addRoleMapping('medical_manager', ['lab.read']);
-        $registry->addRoleMapping('doctor', ['lab.read', 'lab.write']);
-        $registry->addRoleMapping('nurse', ['lab.read', 'lab.write']);
-        $registry->addRoleMapping('lab_technician', ['lab.read', 'lab.write']);
+        $registry->register('lab_order', LabOrderPolicy::class);
     }
+}
 
     public function registerPolicies(PolicyRegistry $registry): void
     {
