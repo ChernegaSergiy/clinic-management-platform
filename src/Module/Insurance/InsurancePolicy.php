6c5fd4d94910f4096ce90469de1522ec9a21bc28
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-04 10:27:44 +0200

    refactor(insurance): Refactor InsurancePolicy to use new auth API

--- src/Module/Insurance/InsurancePolicy.php
+++ src/Module/Insurance/InsurancePolicy.php
@@ -3,26 +3,12 @@
 namespace App\Module\Insurance;
 
 use App\Core\Policy;
+use App\Core\User;
 
-class InsurancePolicy extends Policy
+class InsurancePolicy implements Policy
 {
-    public function view(mixed $resource = null): bool
+    public function manage(User $user, array $context): bool
     {
-        return $this->isAdmin();
-    }
-
-    public function create(mixed $resource = null): bool
-    {
-        return $this->isAdmin();
-    }
-
-    public function update(mixed $resource = null): bool
-    {
-        return $this->isAdmin();
-    }
-
-    public function delete(mixed $resource = null): bool
-    {
-        return $this->isAdmin();
+        return $user->hasPermission('insurance.manage');
     }
 }
