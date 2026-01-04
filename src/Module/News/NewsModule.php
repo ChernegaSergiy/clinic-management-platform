5bb6e08c251739aeb66c4538c1f566b13f2382fb
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-04 07:50:54 +0200

    feat: Add permissions and policies registration to NewsModule

--- src/Module/News/NewsModule.php
+++ src/Module/News/NewsModule.php
@@ -4,6 +4,8 @@ namespace App\Module\News;
 
 use App\Core\BaseModule;
 use App\Core\Router;
+use App\Core\PermissionRegistry;
+use App\Core\PolicyRegistry;
 use App\Module\News\NewsController;
 
 class NewsModule extends BaseModule
@@ -18,5 +20,17 @@ class NewsModule extends BaseModule
         $router->add('GET', '/admin/news/edit/{id}', [NewsController::class, 'edit']);
         $router->add('POST', '/admin/news/edit/{id}', [NewsController::class, 'update']);
         $router->add('POST', '/admin/news/delete/{id}', [NewsController::class, 'delete']);
+    }
+
+    public function registerPermissions(PermissionRegistry $registry): void
+    {
+        $registry->add('news.read', 'Перегляд новин');
+        $registry->add('news.manage', 'Керування новинами');
+
+        $registry->addRoleMapping('admin', ['news.read', 'news.manage']);
+    }
+
+    public function registerPolicies(PolicyRegistry $registry): void
+    {
     }
 }
