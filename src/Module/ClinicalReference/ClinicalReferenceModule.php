8cd2cf3ecc3568fcec5ef59ed117b7659ccb38a5
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-04 07:51:32 +0200

    feat: Add permissions and policies registration to ClinicalReferenceModule

--- src/Module/ClinicalReference/ClinicalReferenceModule.php
+++ src/Module/ClinicalReference/ClinicalReferenceModule.php
@@ -4,6 +4,8 @@ namespace App\Module\ClinicalReference;
 
 use App\Core\BaseModule;
 use App\Core\Router;
+use App\Core\PermissionRegistry;
+use App\Core\PolicyRegistry;
 use App\Module\ClinicalReference\ClinicalReferenceController;
 
 class ClinicalReferenceModule extends BaseModule
@@ -14,6 +16,18 @@ class ClinicalReferenceModule extends BaseModule
         $router->add('GET', '/admin/clinical/icd-import', [ClinicalReferenceController::class, 'icdImportForm']);
         $router->add('POST', '/admin/clinical/icd-import', [ClinicalReferenceController::class, 'icdImportRun']);
         $router->add('GET', '/admin/clinical/intervention-import', [ClinicalReferenceController::class, 'interventionImportForm']);
-        $router->add('POST', '/admin/clinical/intervention-import', [ClinicalReferenceController::class, 'interventionImportRun']);
+            $router->add('POST', '/admin/clinical/intervention-import', [ClinicalReferenceController::class, 'interventionImportRun']);
+    }
+
+    public function registerPermissions(PermissionRegistry $registry): void
+    {
+        $registry->add('clinical.manage', 'Керування клінічними довідниками');
+
+        $registry->addRoleMapping('admin', ['clinical.manage']);
+        $registry->addRoleMapping('medical_manager', ['clinical.manage']);
+    }
+
+    public function registerPolicies(PolicyRegistry $registry): void
+    {
     }
 }
