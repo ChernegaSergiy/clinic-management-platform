c1530eb0a56bdb3d365f622f09a8a18cc78b718b
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-04 08:12:25 +0200

    refactor: Update KpiRepository import path in DashboardService

--- src/Module/Dashboard/Service/DashboardService.php
+++ src/Module/Dashboard/Service/DashboardService.php
@@ -2,7 +2,7 @@
 
 namespace App\Module\Dashboard\Service;
 
-use App\Module\Admin\Repository\KpiRepository;
+use App\Module\Kpi\Repository\KpiRepository;
 use App\Module\Billing\Repository\InvoiceRepository;
 use App\Module\Patient\Repository\PatientRepository;
 use App\Module\Appointment\Repository\AppointmentRepository;
