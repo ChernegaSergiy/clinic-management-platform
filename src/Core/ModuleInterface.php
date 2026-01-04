2ba85af53905a9b0a06d16af3ade75cfe62c7f9e
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-04 07:47:09 +0200

    feat: Add registerPermissions and registerPolicies methods to ModuleInterface

--- src/Core/ModuleInterface.php
+++ src/Core/ModuleInterface.php
@@ -11,4 +11,8 @@ interface ModuleInterface
     public function bootstrap(): void;
 
     public function registerRoutes(Router $router): void;
+
+    public function registerPermissions(PermissionRegistry $registry): void;
+
+    public function registerPolicies(PolicyRegistry $registry): void;
 }
