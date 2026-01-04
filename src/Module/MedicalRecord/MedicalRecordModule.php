e86b6d7c7080e4dd264ea16c06f85acc6d65fefb
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-04 07:49:12 +0200

    feat: Add permissions and policies registration to MedicalRecordModule

--- src/Module/MedicalRecord/MedicalRecordModule.php
+++ src/Module/MedicalRecord/MedicalRecordModule.php
@@ -4,6 +4,8 @@ namespace App\Module\MedicalRecord;
 
 use App\Core\BaseModule;
 use App\Core\Router;
+use App\Core\PermissionRegistry;
+use App\Core\PolicyRegistry;
 use App\Module\MedicalRecord\MedicalRecordController;
 
 class MedicalRecordModule extends BaseModule
@@ -20,5 +22,20 @@ class MedicalRecordModule extends BaseModule
         $router->add('GET', '/medical-records/attachments/download', [MedicalRecordController::class, 'downloadAttachment']);
         $router->add('GET', '/medical-records/icd-codes', [MedicalRecordController::class, 'getIcdCodes']);
         $router->add('GET', '/medical-records/intervention-codes', [MedicalRecordController::class, 'getInterventionCodes']);
+    }
+
+    public function registerPermissions(PermissionRegistry $registry): void
+    {
+        $registry->add('medical.read', 'Перегляд медичних записів');
+        $registry->add('medical.write', 'Редагування медичних записів');
+
+        $registry->addRoleMapping('admin', ['medical.read', 'medical.write']);
+        $registry->addRoleMapping('medical_manager', ['medical.read', 'medical.write']);
+        $registry->addRoleMapping('doctor', ['medical.read', 'medical.write']);
+        $registry->addRoleMapping('nurse', ['medical.read', 'medical.write']);
+    }
+
+    public function registerPolicies(PolicyRegistry $registry): void
+    {
     }
 }
