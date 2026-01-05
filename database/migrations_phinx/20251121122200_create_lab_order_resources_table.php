5787860cd4fdd229239b5141a42b33fe62109e12
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-05 21:40:30 +0000

    fix: Add NOT NULL constraint to lab_order_resources primary key columns

--- database/migrations_phinx/20251121122200_create_lab_order_resources_table.php
+++ database/migrations_phinx/20251121122200_create_lab_order_resources_table.php
@@ -7,8 +7,8 @@ class CreateLabOrderResourcesTable extends AbstractMigration
     public function change()
     {
         $table = $this->table('lab_order_resources', ['id' => false, 'primary_key' => ['lab_order_id', 'lab_resource_id']]);
-        $table->addColumn('lab_order_id', 'integer', ['signed' => false])
-              ->addColumn('lab_resource_id', 'integer', ['signed' => false])
+        $table->addColumn('lab_order_id', 'integer', ['signed' => false, 'null' => false])
+              ->addColumn('lab_resource_id', 'integer', ['signed' => false, 'null' => false])
               ->addForeignKey('lab_order_id', 'lab_orders', 'id', ['delete' => 'CASCADE', 'update' => 'NO_ACTION'])
               ->addForeignKey('lab_resource_id', 'lab_resources', 'id', ['delete' => 'CASCADE', 'update' => 'NO_ACTION'])
               ->addIndex(['lab_order_id'])
