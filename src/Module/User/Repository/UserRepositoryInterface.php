6a056c81ca7048e2ce3b4f8a9193c3793fc7b190
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-31 03:56:21 +0200

    refactor: Change UserRepository save method return type

--- src/Module/User/Repository/UserRepositoryInterface.php
+++ src/Module/User/Repository/UserRepositoryInterface.php
@@ -9,7 +9,7 @@ interface UserRepositoryInterface
     public function findByEmailExcludingId(string $email, int $id): ?array;
     public function findAllDoctors(): array;
     public function findAllActive(): array;
-    public function save(array $data): bool;
+    public function save(array $data): int|false;
     public function update(int $id, array $data): bool;
     public function delete(int $id): bool;
     public function countUsers(): int;
