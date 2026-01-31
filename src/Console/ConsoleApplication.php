bf1271a942b71d50d6b92cafec8af78329e6cc78
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-31 02:53:04 +0200

    feat: Create ConsoleApplication class for CLI entry point

---
+++ src/Console/ConsoleApplication.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Console;
+
+use App\Console\Command\CalculateKpisCommand;
+use App\Console\Command\MigrateCommand;
+use Symfony\Component\Console\Application;
+
+class ConsoleApplication extends Application
+{
+    public function __construct()
+    {
+        parent::__construct('Clinic Management Platform', '1.0.0');
+
+        // Register commands here
+        $this->add(new CalculateKpisCommand());
+    }
+}
