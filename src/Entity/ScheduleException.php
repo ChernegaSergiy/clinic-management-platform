b13c422c290d042a4c053d535d80de9b7dbff387
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-24 21:49:45 +0300

    feat: Add ScheduleException entity

---
+++ src/Entity/ScheduleException.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Entity;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'schedule_exceptions')]
+class ScheduleException
+{
+    #[ORM\Id] #[ORM\GeneratedValue] #[ORM\Column] private ?int $id = null;
+    #[ORM\Column(type: Types::INTEGER)] private ?int $doctor_id = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)] private ?\DateTimeInterface $exception_date = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)] private ?\DateTimeInterface $start_time = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)] private ?\DateTimeInterface $end_time = null;
+    #[ORM\Column(type: Types::BOOLEAN)] private bool $is_available = true;
+    #[ORM\Column(type: Types::TEXT, nullable: true)] private ?string $notes = null;
+}
