8e3150b99c67f8b3f00dce65f102d0ebe35b6968
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-24 19:57:49 +0300

    feat: Add SiteModule to configure routes and DI for site pages

---
+++ src/Module/Site/SiteModule.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Module\Site;
+
+use App\Core\Module\BaseModule;
+use App\Core\Http\Router;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class SiteModule extends BaseModule
+{
+    public function registerRoutes(Router $router): void
+    {
+        $router->add('GET', '/', [SiteController::class, 'home']);
+        $router->add('GET', '/about', [SiteController::class, 'about']);
+        $router->add('GET', '/our-team', [SiteController::class, 'ourTeam']);
+        $router->add('GET', '/contact', [SiteController::class, 'contact']);
+        $router->add('GET', '/sitemap', [SiteController::class, 'sitemap']);
+        $router->add('GET', '/privacy', [SiteController::class, 'privacy']);
+        $router->add('GET', '/departments', [SiteController::class, 'departments']);
+        $router->add('GET', '/doctors', [SiteController::class, 'doctors']);
+    }
+
+    public function registerServices(ContainerBuilder $container): void
+    {
+        $container->register(SiteController::class)->setPublic(true);
+    }
+}
