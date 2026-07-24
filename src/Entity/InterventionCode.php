02bdf91ce7537746fc62ee0cbf6e98ab19885aa7
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-24 21:49:45 +0300

    feat: Add InterventionCode entity

---
+++ src/Entity/InterventionCode.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Entity;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'intervention_codes')]
+class InterventionCode
+{
+    #[ORM\Id] #[ORM\GeneratedValue] #[ORM\Column] private ?int $id = null;
+    #[ORM\Column(length: 50)] private ?string $code = null;
+    #[ORM\Column(length: 255)] private ?string $description = null;
+}
