bc97cf12a6c5bb6a5ff281b855d41c1b14782bea
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-24 21:49:45 +0300

    feat: Add PatientInsurancePolicy entity

---
+++ src/Entity/PatientInsurancePolicy.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Entity;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'patient_insurance_policies')]
+class PatientInsurancePolicy
+{
+    #[ORM\Id] #[ORM\GeneratedValue] #[ORM\Column] private ?int $id = null;
+    #[ORM\Column(type: Types::INTEGER)] private ?int $patient_id = null;
+    #[ORM\Column(type: Types::INTEGER)] private ?int $insurance_company_id = null;
+    #[ORM\Column(length: 255)] private ?string $policy_number = null;
+    #[ORM\Column(length: 255, nullable: true)] private ?string $group_number = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)] private ?\DateTimeInterface $valid_from = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)] private ?\DateTimeInterface $valid_to = null;
+    #[ORM\Column(type: Types::BOOLEAN)] private bool $is_active = true;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)] private ?\DateTimeInterface $created_at = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)] private ?\DateTimeInterface $updated_at = null;
+}
