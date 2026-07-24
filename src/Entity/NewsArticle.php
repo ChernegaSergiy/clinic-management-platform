1023d122d4c64dfa8ed0a9f78b9ca4e5ea51d47c
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-24 21:49:45 +0300

    feat: Add NewsArticle entity

---
+++ src/Entity/NewsArticle.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Entity;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'news_articles')]
+class NewsArticle
+{
+    #[ORM\Id] #[ORM\GeneratedValue] #[ORM\Column] private ?int $id = null;
+    #[ORM\Column(length: 255)] private ?string $title = null;
+    #[ORM\Column(type: Types::TEXT, nullable: true)] private ?string $meta = null;
+    #[ORM\Column(type: Types::TEXT)] private ?string $content = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)] private ?\DateTimeInterface $published_at = null;
+    #[ORM\Column(type: Types::INTEGER)] private ?int $author_id = null;
+    #[ORM\Column(type: Types::BOOLEAN)] private bool $is_published = false;
+}
