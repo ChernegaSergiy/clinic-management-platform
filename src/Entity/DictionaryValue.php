997af2fc9463aca91cd3357678c86f5c56ade5ee
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-24 21:49:45 +0300

    feat: Add DictionaryValue entity

---
+++ src/Entity/DictionaryValue.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Entity;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'dictionary_values')]
+class DictionaryValue
+{
+    #[ORM\Id] #[ORM\GeneratedValue] #[ORM\Column] private ?int $id = null;
+    #[ORM\Column(type: Types::INTEGER)] private ?int $dictionary_id = null;
+    #[ORM\Column(length: 255)] private ?string $value = null;
+    #[ORM\Column(length: 255)] private ?string $label = null;
+    #[ORM\Column(type: Types::INTEGER, nullable: true)] private ?int $order_num = null;
+    #[ORM\Column(type: Types::BOOLEAN)] private bool $is_active = true;
+}
