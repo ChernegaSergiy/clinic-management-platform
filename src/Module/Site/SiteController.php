eee80ccf36c7e0174935352cf441595e3fbd955a
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-24 21:32:43 +0300

    feat: Convert SiteController to standard Symfony routing and Response

--- src/Module/Site/SiteController.php
+++ src/Module/Site/SiteController.php
@@ -3,48 +3,58 @@
 namespace App\Module\Site;
 
 use App\Core\Http\View;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Yaml\Yaml;
 
 class SiteController
 {
-    public function home(): void
+    #[Route('/', name: 'site_home')]
+    public function home(): Response
     {
         $content = Yaml::parseFile(__DIR__ . '/../../../content/home.uk.yml');
-        View::render('@modules/Site/templates/home/index.html.twig', ['page' => $content]);
+        return new Response(View::renderToString('@modules/Site/templates/home/index.html.twig', ['page' => $content]));
     }
 
-    public function about(): void
+    #[Route('/about', name: 'site_about')]
+    public function about(): Response
     {
-        View::render('@modules/Site/templates/about/index.html.twig');
+        return new Response(View::renderToString('@modules/Site/templates/about/index.html.twig'));
     }
 
-    public function contact(): void
+    #[Route('/contact', name: 'site_contact')]
+    public function contact(): Response
     {
-        View::render('@modules/Site/templates/contact/index.html.twig');
+        return new Response(View::renderToString('@modules/Site/templates/contact/index.html.twig'));
     }
 
-    public function sitemap(): void
+    #[Route('/sitemap', name: 'site_sitemap')]
+    public function sitemap(): Response
     {
-        View::render('@modules/Site/templates/sitemap.html.twig');
+        return new Response(View::renderToString('@modules/Site/templates/sitemap.html.twig'));
     }
 
-    public function privacy(): void
+    #[Route('/privacy', name: 'site_privacy')]
+    public function privacy(): Response
     {
-        View::render('@modules/Site/templates/privacy.html.twig');
+        return new Response(View::renderToString('@modules/Site/templates/privacy.html.twig'));
     }
 
-    public function departments(): void
+    #[Route('/departments', name: 'site_departments')]
+    public function departments(): Response
     {
-        View::render('@modules/Site/templates/departments/index.html.twig');
+        return new Response(View::renderToString('@modules/Site/templates/departments/index.html.twig'));
     }
 
-    public function doctors(): void
+    #[Route('/doctors', name: 'site_doctors')]
+    public function doctors(): Response
     {
-        View::render('@modules/Site/templates/doctors/index.html.twig');
+        return new Response(View::renderToString('@modules/Site/templates/doctors/index.html.twig'));
     }
 
-    public function ourTeam(): void
+    #[Route('/our-team', name: 'site_our_team')]
+    public function ourTeam(): Response
     {
-        View::render('@modules/Site/templates/our-team/index.html.twig');
+        return new Response(View::renderToString('@modules/Site/templates/our-team/index.html.twig'));
     }
 }
