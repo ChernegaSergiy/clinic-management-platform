2202dfaf2791a9ac30f02dadaab32c695b771ccd
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-24 21:49:45 +0300

    feat: Add Invoice entity

---
+++ src/Entity/Invoice.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Entity;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'invoices')]
+class Invoice
+{
+    #[ORM\Id] #[ORM\GeneratedValue] #[ORM\Column] private ?int $id = null;
+    #[ORM\Column(type: Types::INTEGER)] private ?int $patient_id = null;
+    #[ORM\Column(type: Types::INTEGER, nullable: true)] private ?int $appointment_id = null;
+    #[ORM\Column(type: Types::INTEGER, nullable: true)] private ?int $medical_record_id = null;
+    #[ORM\Column(type: Types::FLOAT)] private ?float $amount = null;
+    #[ORM\Column(length: 50)] private ?string $status = null;
+    #[ORM\Column(type: Types::TEXT, nullable: true)] private ?string $notes = null;
+    #[ORM\Column(length: 50, nullable: true)] private ?string $type = null;
+}
