9ab092b75c1892c80c088bc1eb1349293f7f0b41
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-24 21:49:45 +0300

    feat: Add KpiDefinition entity

---
+++ src/Entity/KpiDefinition.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Entity;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'kpi_definitions')]
+class KpiDefinition
+{
+    #[ORM\Id] #[ORM\GeneratedValue] #[ORM\Column] private ?int $id = null;
+    #[ORM\Column(length: 255)] private ?string $name = null;
+    #[ORM\Column(type: Types::TEXT, nullable: true)] private ?string $description = null;
+    #[ORM\Column(length: 50)] private ?string $kpi_type = null;
+    #[ORM\Column(type: Types::FLOAT, nullable: true)] private ?float $target_value = null;
+    #[ORM\Column(length: 50, nullable: true)] private ?string $unit = null;
+    #[ORM\Column(type: Types::BOOLEAN)] private bool $is_active = true;
+    #[ORM\Column(length: 50, nullable: true)] private ?string $period = null;
+}
