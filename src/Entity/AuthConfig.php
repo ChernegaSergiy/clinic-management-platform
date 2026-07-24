ecb91ec646b206fa69ca78343e3082233d35abfc
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-24 21:49:45 +0300

    feat: Add AuthConfig entity

---
+++ src/Entity/AuthConfig.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Entity;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'auth_configs')]
+class AuthConfig
+{
+    #[ORM\Id] #[ORM\GeneratedValue] #[ORM\Column] private ?int $id = null;
+    #[ORM\Column(length: 255)] private ?string $provider = null;
+    #[ORM\Column(length: 255, nullable: true)] private ?string $client_id = null;
+    #[ORM\Column(length: 255, nullable: true)] private ?string $client_secret = null;
+    #[ORM\Column(type: Types::BOOLEAN)] private bool $is_active = true;
+    #[ORM\Column(type: Types::TEXT, nullable: true)] private ?string $config = null;
+}
