46cfdd4f7ebe57205aea65abd258ac2ab518b40c
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-24 21:49:45 +0300

    feat: Add ServiceCategory entity

---
+++ src/Entity/ServiceCategory.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Entity;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'service_categories')]
+class ServiceCategory
+{
+    #[ORM\Id] #[ORM\GeneratedValue] #[ORM\Column] private ?int $id = null;
+    #[ORM\Column(length: 255)] private ?string $name = null;
+    #[ORM\Column(type: Types::TEXT, nullable: true)] private ?string $description = null;
+}
