a7109eb7da3d64a23e9873595a1fbdd2d8d9010b
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-25 10:48:11 +0300

    fix: Add missing columns to Invoice entity

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -16,4 +16,10 @@ class Invoice
     #[ORM\Column(length: 50)] private ?string $status = null;
     #[ORM\Column(type: Types::TEXT, nullable: true)] private ?string $notes = null;
     #[ORM\Column(length: 50, nullable: true)] private ?string $type = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)] private ?\DateTimeInterface $issued_date = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)] private ?\DateTimeInterface $paid_date = null;
+    #[ORM\Column(type: Types::FLOAT, nullable: true)] private ?float $insurance_due = null;
+    #[ORM\Column(type: Types::FLOAT, nullable: true)] private ?float $patient_due = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)] private ?\DateTimeInterface $created_at = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)] private ?\DateTimeInterface $updated_at = null;
 }
