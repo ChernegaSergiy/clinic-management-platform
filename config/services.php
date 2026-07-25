a441a68d57402b6fe25f0d7c0e3f9d138f694ace
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-25 10:28:25 +0300

    refactor: Alias EventDispatcherInterface to EventDispatcher in DI

--- config/services.php
+++ config/services.php
@@ -23,6 +23,7 @@ return function (ContainerBuilder $container) {
     $container->register(\App\Core\Auth\PermissionRegistry::class)->setPublic(true);
     $container->register(\App\Core\Auth\PolicyRegistry::class)->setPublic(true);
     $container->register(\Symfony\Component\EventDispatcher\EventDispatcher::class)->setPublic(true);
+    $container->setAlias(\Symfony\Contracts\EventDispatcher\EventDispatcherInterface::class, \Symfony\Component\EventDispatcher\EventDispatcher::class);
     $container->register(\App\Core\Service\TranslationService::class)->setPublic(true);
     $container->register(\App\Core\Service\AuditLogger::class)
         ->setArguments([new Reference('pdo')])
