791eed4f2b607f223eb6151f3de4686aedc8f964
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-25 05:32:57 +0300

    refactor: Remove staticContainer from Kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,12 +8,4 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    public static ?\Symfony\Component\DependencyInjection\ContainerInterface $staticContainer = null;
-
-    public function boot(): void
-    {
-        parent::boot();
-        self::$staticContainer = $this->getContainer();
-    }
 }
