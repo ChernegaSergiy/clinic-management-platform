175be0379226ce8165a40ad7a331b326cd342339
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-25 05:32:57 +0300

    feat: Create AbstractController base class

---
+++ src/Core/Controller/AbstractController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Core\Controller;
+
+use App\Core\Auth\AuthGuard;
+use App\Core\Http\View;
+
+abstract class AbstractController
+{
+    protected AuthGuard $authGuard;
+    protected View $view;
+
+    /**
+     * Set dependencies required by the base controller.
+     * This avoids forcing child classes to call parent::__construct with all dependencies.
+     */
+    #[\Symfony\Contracts\Service\Attribute\Required]
+    public function setBaseDependencies(AuthGuard $authGuard, View $view): void
+    {
+        $this->authGuard = $authGuard;
+        $this->view = $view;
+    }
+
+    protected function render(string $template, array $data = []): void
+    {
+        $this->view->render($template, $data);
+    }
+
+    protected function checkAuth(): void
+    {
+        $this->authGuard->check();
+    }
+}
