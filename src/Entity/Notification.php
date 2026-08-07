3af6bc39f890852789fd0dd4d2cd78da3425bec3
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-08 00:10:29 +0300

    refactor(Core): Update legacy imports to point to NotificationBundle before migration

--- src/Entity/Notification.php
+++ src/Entity/Notification.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Module\Notification\Repository\NotificationRepository;
+use App\Bundles\NotificationBundle\Repository\NotificationRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
