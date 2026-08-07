b3a79f5e88f4ea3d91032cd524f207dcf8e1cb70
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 18:33:34 +0300

    feat(Hrm): Create extension for HrmBundle DI

---
+++ src/Bundles/HrmBundle/DependencyInjection/HrmExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundles\HrmBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class HrmExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container) : void {}
+}
