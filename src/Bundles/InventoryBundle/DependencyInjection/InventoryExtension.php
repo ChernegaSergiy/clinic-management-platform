318872b737937978c36c2cc062b82cc5fe7cb893
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-08 00:12:52 +0300

    feat(Inventory): Create extension for InventoryBundle DI

---
+++ src/Bundles/InventoryBundle/DependencyInjection/InventoryExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundles\InventoryBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class InventoryExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container) : void {}
+}
