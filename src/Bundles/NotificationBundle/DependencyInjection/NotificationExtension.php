0876bea567ce5a57f5914cade4b9cdb4b79f6059
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-08 00:07:53 +0300

    feat(Notification): Create extension for NotificationBundle DI

---
+++ src/Bundles/NotificationBundle/DependencyInjection/NotificationExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundles\NotificationBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class NotificationExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container) : void {}
+}
