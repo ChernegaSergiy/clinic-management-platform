e6998ff2d7907bb1d722c8f36dc44f2707f22d89
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 15:31:14 +0300

    feat(Insurance): Create extension for InsuranceBundle DI

---
+++ src/Bundles/InsuranceBundle/DependencyInjection/InsuranceExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundles\InsuranceBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class InsuranceExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container) : void {}
+}
