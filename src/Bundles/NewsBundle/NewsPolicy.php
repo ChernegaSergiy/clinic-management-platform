0bafbaad1de3226fdc04250ce2f6a6982a626d96
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-08 00:29:57 +0300

    refactor(News): Move Repository and Policy to bundle and update namespaces

---
+++ src/Bundles/NewsBundle/NewsPolicy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Bundles\NewsBundle;
+
+use App\Core\Auth\Policy;
+use App\Core\Model\User;
+
+class NewsPolicy implements Policy
+{
+    public function view(User $user, array $context) : bool
+    {
+        return $user->hasPermission('news.read');
+    }
+
+    public function create(User $user, array $context) : bool
+    {
+        return $user->hasPermission('news.manage');
+    }
+
+    public function update(User $user, array $context) : bool
+    {
+        return $user->hasPermission('news.manage');
+    }
+
+    public function delete(User $user, array $context) : bool
+    {
+        return $user->hasPermission('news.manage');
+    }
+}
