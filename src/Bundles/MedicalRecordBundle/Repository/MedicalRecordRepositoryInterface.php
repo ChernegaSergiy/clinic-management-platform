d0ced2c66d807a87bc4d0a02f9e567204ff53c14
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 14:13:44 +0300

    refactor(MedicalRecord): Move Repository to bundle and update namespaces

---
+++ src/Bundles/MedicalRecordBundle/Repository/MedicalRecordRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Bundles\MedicalRecordBundle\Repository;
+
+interface MedicalRecordRepositoryInterface
+{
+    public function findByPatientId(int $patientId) : array;
+    public function findByDoctorId(int $doctorId) : array;
+    public function findAll() : array;
+    public function save(array $data) : int|false;
+    public function findById(int $id) : ?array;
+    // public function update(int $id, array $data): bool;
+    // public function delete(int $id): bool;
+}
