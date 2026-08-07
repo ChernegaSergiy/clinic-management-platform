329a5673b332d5033faa8d1ccfebd41d8dddd5dc
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-08 00:01:21 +0300

    feat(ClinicalReference): Create extension for ClinicalReferenceBundle DI

---
+++ src/Bundles/ClinicalReferenceBundle/DependencyInjection/ClinicalReferenceExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundles\ClinicalReferenceBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class ClinicalReferenceExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container) : void {}
+}
