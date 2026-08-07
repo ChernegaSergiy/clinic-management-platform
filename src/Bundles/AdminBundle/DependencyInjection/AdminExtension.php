400eefe313af41ae1e736b8018187ac5a03fcee0
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-08 00:18:36 +0300

    feat(Admin): Create extension for AdminBundle DI

---
+++ src/Bundles/AdminBundle/DependencyInjection/AdminExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundles\AdminBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class AdminExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container) : void {}
+}
