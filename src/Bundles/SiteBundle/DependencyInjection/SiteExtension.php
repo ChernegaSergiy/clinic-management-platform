22a4da9d0a2ae2b41ff20aac31d34f723b7a0c87
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-08 00:31:21 +0300

    feat(Site): Create extension for SiteBundle DI

---
+++ src/Bundles/SiteBundle/DependencyInjection/SiteExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundles\SiteBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class SiteExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container) : void {}
+}
