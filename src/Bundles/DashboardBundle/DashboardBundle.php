756f79bdfc065b766b8ae775d47b2e161e8dfb0c
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 12:23:26 +0300

    refactor(Dashboard): Register permissions Compiler Pass in bundle

--- src/Bundles/DashboardBundle/DashboardBundle.php
+++ src/Bundles/DashboardBundle/DashboardBundle.php
@@ -2,6 +2,15 @@
 
 namespace App\Bundles\DashboardBundle;
 
+use App\Bundles\DashboardBundle\DependencyInjection\Compiler\DashboardPermissionsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class DashboardBundle extends Bundle {}
+class DashboardBundle extends Bundle
+{
+    public function build(ContainerBuilder $container) : void
+    {
+        parent::build($container);
+        $container->addCompilerPass(new DashboardPermissionsPass());
+    }
+}
