61ee0147b6b79e853c64f75ddd0306c55c5dc93f
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 08:39:51 +0300

    test: Rewrite RepositoryTestCase for Doctrine ORM mocking

--- tests/RepositoryTestCase.php
+++ tests/RepositoryTestCase.php
@@ -2,28 +2,66 @@
 
 namespace App\Tests;
 
-use PDO;
+use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 abstract class RepositoryTestCase extends TestCase
 {
-    protected PDO $pdo;
+    /** @var EntityManagerInterface&MockObject */
+    protected $entityManager;
+
+    /** @var ManagerRegistry&MockObject */
+    protected $managerRegistry;
 
     protected function setUp() : void
     {
-        $this->pdo = $this->createMockPdo();
+        $this->entityManager = $this->createMock(EntityManagerInterface::class);
+        $this->managerRegistry = $this->createMock(ManagerRegistry::class);
     }
 
-    private function createMockPdo() : PDO
+    protected function createMockManagerRegistry(string $entityClass) : ManagerRegistry&MockObject
     {
-        $pdo = $this->createMock(PDO::class);
+        $this->managerRegistry->method('getManagerForClass')
+            ->with($entityClass)
+            ->willReturn($this->entityManager);
 
-        $pdo->method('prepare')
-            ->willReturnCallback(function ($sql) {
-                $stmt = $this->createMock(\PDOStatement::class);
-                return $stmt;
-            });
+        $this->entityManager->method('getClassMetadata')
+            ->with($entityClass)
+            ->willReturn(new ClassMetadata($entityClass));
 
-        return $pdo;
+        return $this->managerRegistry;
+    }
+
+    protected function createMockQueryBuilder(mixed $result = null, bool $isSingleResult = false) : QueryBuilder&MockObject
+    {
+        $queryBuilder = $this->createMock(QueryBuilder::class);
+        $query = $this->createMock(AbstractQuery::class);
+
+        if ($isSingleResult) {
+            $query->method('getOneOrNullResult')->willReturn($result);
+        } else {
+            $query->method('getResult')->willReturn($result ?? []);
+        }
+
+        $queryBuilder->method('getQuery')->willReturn($query);
+
+        // Allow method chaining
+        $queryBuilder->method('select')->willReturnSelf();
+        $queryBuilder->method('from')->willReturnSelf();
+        $queryBuilder->method('where')->willReturnSelf();
+        $queryBuilder->method('andWhere')->willReturnSelf();
+        $queryBuilder->method('orWhere')->willReturnSelf();
+        $queryBuilder->method('setParameter')->willReturnSelf();
+        $queryBuilder->method('orderBy')->willReturnSelf();
+        $queryBuilder->method('addOrderBy')->willReturnSelf();
+        $queryBuilder->method('leftJoin')->willReturnSelf();
+        $queryBuilder->method('innerJoin')->willReturnSelf();
+
+        return $queryBuilder;
     }
 }
