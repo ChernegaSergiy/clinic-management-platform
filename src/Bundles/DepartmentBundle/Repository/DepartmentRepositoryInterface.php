ca0e6f12b233457a8e2767355f2b705d425abecf
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 15:11:57 +0300

    refactor(Department): Move Repository to bundle and update namespaces

---
+++ src/Bundles/DepartmentBundle/Repository/DepartmentRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Bundles\DepartmentBundle\Repository;
+
+interface DepartmentRepositoryInterface
+{
+    public function findAll() : array;
+    public function findAllActive() : array;
+    public function findById(int $id) : ?array;
+    public function save(array $data) : bool;
+    public function update(int $id, array $data) : bool;
+    public function delete(int $id) : bool;
+    public function findByName(string $name) : ?array;
+    public function getHierarchy() : array;
+}
