3b51b996a6fc3a8bbeb3ff47934dd96364fe5b45
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-08 02:58:06 +0300

    chore: Add MedCore CSSM License header to all PHP source files

--- src/Bundles/DepartmentBundle/DependencyInjection/DepartmentExtension.php
+++ src/Bundles/DepartmentBundle/DependencyInjection/DepartmentExtension.php
@@ -1,5 +1,27 @@
 <?php
 
+/*
+ *
+ *                      _
+ *   _ __ ___   ___  __| | ___ ___  _ __ ___       _   _  __ _
+ *  | '_ ` _ \ / _ \/ _` |/ __/ _ \| '__/ _ \_____| | | |/ _` |
+ *  | | | | | |  __/ (_| | (_| (_) | | |  __/_____| |_| | (_| |
+ *  |_| |_| |_|\___|\__,_|\___\___/|_|  \___|      \__,_|\__,_|
+ *
+ * This program is free software: you can redistribute and/or modify
+ * it under the terms of the CSSM Unlimited License v2.0.
+ *
+ * This license permits unlimited use, modification, and distribution
+ * for any purpose while maintaining authorship attribution.
+ *
+ * The software is provided "as is" without warranty of any kind.
+ *
+ * @author MedCore Ukraine
+ * @link https://medcore.pp.ua/
+ *
+ *
+ */
+
 namespace App\Bundles\DepartmentBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
