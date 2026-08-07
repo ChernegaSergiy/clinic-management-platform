a045a2cb4975ab71366093c0c6ce2cca939a201e
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 14:22:14 +0300

    feat(Room): Add CompilerPass for room permissions

---
+++ src/Bundles/RoomBundle/DependencyInjection/Compiler/RoomPermissionsPass.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Bundles\RoomBundle\DependencyInjection\Compiler;
+
+use App\Bundles\RoomBundle\RoomPolicy;
+use App\Core\Auth\PermissionRegistry;
+use App\Core\Auth\PolicyRegistry;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class RoomPermissionsPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container) : void
+    {
+        if ($container->hasDefinition(PermissionRegistry::class)) {
+            $registry = $container->getDefinition(PermissionRegistry::class);
+            $registry->addMethodCall('add', ['rooms.manage', 'Керування приміщеннями']);
+
+            $registry->addMethodCall('addRoleMapping', ['admin', ['rooms.manage']]);
+        }
+
+        if ($container->hasDefinition(PolicyRegistry::class)) {
+            $registry = $container->getDefinition(PolicyRegistry::class);
+            $registry->addMethodCall('register', ['rooms', RoomPolicy::class]);
+        }
+    }
+}
