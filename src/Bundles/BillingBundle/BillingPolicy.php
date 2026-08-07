591c5ea396a7ccf2ab15989dcc99a206a62b18cd
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 18:30:33 +0300

    refactor(Billing): Move Policy to bundle and update namespaces

---
+++ src/Bundles/BillingBundle/BillingPolicy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Bundles\BillingBundle;
+
+use App\Core\Auth\Policy;
+use App\Core\Model\User;
+
+class BillingPolicy implements Policy
+{
+    public function view(User $user, array $context) : bool
+    {
+        return $user->hasPermission('billing.read');
+    }
+
+    public function create(User $user, array $context) : bool
+    {
+        return $user->hasPermission('billing.manage');
+    }
+
+    public function update(User $user, array $context) : bool
+    {
+        return $user->hasPermission('billing.manage');
+    }
+
+    public function delete(User $user, array $context) : bool
+    {
+        return $user->hasPermission('billing.manage');
+    }
+}
