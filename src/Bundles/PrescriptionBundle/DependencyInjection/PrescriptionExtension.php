902afd233b5f0d53b9035edb34c518c8fb2006ae
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 14:33:11 +0300

    feat(Prescription): Create extension for PrescriptionBundle DI

---
+++ src/Bundles/PrescriptionBundle/DependencyInjection/PrescriptionExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundles\PrescriptionBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class PrescriptionExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container) : void {}
+}
