768509d9269f488d60f09e44f75118e81383e70c
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 14:47:21 +0300

    feat(LabOrder): Create extension for LabOrderBundle DI

---
+++ src/Bundles/LabOrderBundle/DependencyInjection/LabOrderExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundles\LabOrderBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class LabOrderExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container) : void {}
+}
