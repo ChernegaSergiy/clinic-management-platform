d17200586143ba0239faec1bcc644136e33ca2af
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 12:33:26 +0300

    feat(Core): Register PatientBundle in application kernel

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     App\Bundles\DashboardBundle\DashboardBundle::class => ['all' => true],
+    App\Bundles\PatientBundle\PatientBundle::class => ['all' => true],
 ];
