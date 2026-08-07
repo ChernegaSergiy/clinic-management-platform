fa1d7aa97f85b701b8bcbc887b38b100b9e4d8a2
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 18:22:32 +0300

    feat(Billing): Create extension for BillingBundle DI

---
+++ src/Bundles/BillingBundle/DependencyInjection/BillingExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundles\BillingBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class BillingExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container) : void {}
+}
