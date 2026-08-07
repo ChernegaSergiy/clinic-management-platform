41364a8a8e7823ef2b3fb9b1a1107885fec498d7
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-08 00:25:01 +0300

    feat(News): Create extension for NewsBundle DI

---
+++ src/Bundles/NewsBundle/DependencyInjection/NewsExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundles\NewsBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class NewsExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container) : void {}
+}
