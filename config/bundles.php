ad7e51d8444c0ddfa75f017ad040338ab3c91dc3
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 12:47:10 +0300

    feat(Core): Register AppointmentBundle in application kernel

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     App\Bundles\DashboardBundle\DashboardBundle::class => ['all' => true],
     App\Bundles\PatientBundle\PatientBundle::class => ['all' => true],
+    App\Bundles\AppointmentBundle\AppointmentBundle::class => ['all' => true],
 ];
