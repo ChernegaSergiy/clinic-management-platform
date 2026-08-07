19d32b9651c166a2803c7f20e8dd49e29c8ff65e
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-08 00:29:10 +0300

    refactor(Core): Update legacy imports to point to NewsBundle before migration

--- src/Entity/NewsArticle.php
+++ src/Entity/NewsArticle.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Module\News\Repository\NewsRepository;
+use App\Bundles\NewsBundle\Repository\NewsRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
