1dda46cad5364588050cf00ddd12d3b2ad0952ee
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-07 13:48:41 +0300

    feat(Schedule): Create extension for ScheduleBundle DI

---
+++ src/Bundles/ScheduleBundle/DependencyInjection/ScheduleExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundles\ScheduleBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class ScheduleExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container) : void {}
+}
