4fbfe9e58f6f51a2af5d1e276f4c07d3b12a6686
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-08 22:30:43 +0300

    refactor: Migrate NewsBundle role hierarchy to PrependExtensionInterface

--- src/Bundles/NewsBundle/DependencyInjection/NewsExtension.php
+++ src/Bundles/NewsBundle/DependencyInjection/NewsExtension.php
@@ -26,8 +26,23 @@ namespace App\Bundles\NewsBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
-class NewsExtension extends Extension
+class NewsExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container) : void {}
+
+    public function prepend(\Symfony\Component\DependencyInjection\ContainerBuilder $container) : void
+    {
+        $roleHierarchy = [
+            'ROLE_ADMIN' => [
+                'ROLE_NEWS_READ',
+                'ROLE_NEWS_MANAGE',
+            ],
+        ];
+
+        $container->prependExtensionConfig('security', [
+            'role_hierarchy' => $roleHierarchy,
+        ]);
+    }
 }
--- src/Bundles/NewsBundle/NewsBundle.php
+++ src/Bundles/NewsBundle/NewsBundle.php
@@ -24,7 +24,6 @@
 
 namespace App\Bundles\NewsBundle;
 
-use App\Bundles\NewsBundle\DependencyInjection\Compiler\NewsPermissionsPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -33,6 +32,5 @@ class NewsBundle extends Bundle
     public function build(ContainerBuilder $container) : void
     {
         parent::build($container);
-        $container->addCompilerPass(new NewsPermissionsPass());
-    }
+        }
 }
