25a30b53204b418c299b2861c2c30d9eebabd716
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-08 16:29:01 +0300

    refactor: Remove deprecated export method from ExcelExporter

--- src/Core/Export/ExcelExporter.php
+++ src/Core/Export/ExcelExporter.php
@@ -29,23 +29,6 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class ExcelExporter
 {
-    public function export(array $headers, array $data, string $filename = 'export.xlsx') : void
-    {
-        $spreadsheet = new Spreadsheet();
-        $sheet = $spreadsheet->getActiveSheet();
-
-        $sheet->fromArray($headers, null, 'A1');
-        $sheet->fromArray($data, null, 'A2');
-
-        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="' . $filename . '"');
-        header('Cache-Control: max-age=0');
-
-        $writer = new Xlsx($spreadsheet);
-        $writer->save('php://output');
-        exit;
-    }
-
     public function generate(array $headers, array $data) : string
     {
         $spreadsheet = new Spreadsheet();
