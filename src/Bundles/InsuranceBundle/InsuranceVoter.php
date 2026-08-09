ac2e6f81cee311f1835dbd2f02eb8fb7f397e30f
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-09 11:59:00 +0300

    refactor: Add basic security roles check to InsuranceVoter

--- src/Bundles/InsuranceBundle/InsuranceVoter.php
+++ src/Bundles/InsuranceBundle/InsuranceVoter.php
@@ -2,18 +2,39 @@
 
 namespace App\Bundles\InsuranceBundle;
 
+use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 class InsuranceVoter extends Voter
 {
+    public const MANAGE = 'INSURANCE_MANAGE';
+
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     protected function supports(string $attribute, mixed $subject) : bool
     {
-        return false;
+        return self::MANAGE === $attribute;
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token) : bool
     {
+        $user = $token->getUser();
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        // Administrators and Medical Managers can manage insurance providers
+        if ($this->security->isGranted('ROLE_ADMIN') || $this->security->isGranted('ROLE_MEDICAL_MANAGER')) {
+            return true;
+        }
+
         return false;
     }
 }
