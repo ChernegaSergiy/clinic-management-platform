0fc236c8bbd922033d93c485ebce6b389598ad69
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-09 21:11:40 +0300

    feat(NotificationBundle): Add NotificationVoter with NOTIFICATION_VIEW attribute

---
+++ src/Bundles/NotificationBundle/NotificationVoter.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ *
+ *                      _
+ *   _ __ ___   ___  __| | ___ ___  _ __ ___       _   _  __ _
+ *  | '_ ` _ \ / _ \/ _` |/ __/ _ \| '__/ _ \_____| | | |/ _` |
+ *  | | | | | |  __/ (_| | (_| (_) | | |  __/_____| |_| | (_| |
+ *  |_| |_| |_|\___|\__,_|\___\___/|_|  \___|      \__,_|\__,_|
+ *
+ * This program is free software: you can redistribute and/or modify
+ * it under the terms of the CSSM Unlimited License v2.0.
+ *
+ * This license permits unlimited use, modification, and distribution
+ * for any purpose while maintaining authorship attribution.
+ *
+ * The software is provided "as is" without warranty of any kind.
+ *
+ * @author MedCore Ukraine
+ * @link https://medcore.pp.ua/
+ *
+ *
+ */
+
+namespace App\Bundles\NotificationBundle;
+
+use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class NotificationVoter extends Voter
+{
+    public const VIEW = 'NOTIFICATION_VIEW';
+
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    protected function supports(string $attribute, mixed $subject) : bool
+    {
+        return self::VIEW === $attribute;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token) : bool
+    {
+        $user = $token->getUser();
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        if ($this->security->isGranted('ROLE_ADMIN') || $this->security->isGranted('ROLE_MEDICAL_MANAGER')) {
+            return true;
+        }
+
+        return $this->security->isGranted('ROLE_DOCTOR')
+            || $this->security->isGranted('ROLE_NURSE')
+            || $this->security->isGranted('ROLE_REGISTRAR')
+            || $this->security->isGranted('ROLE_BILLING')
+            || $this->security->isGranted('ROLE_LAB_TECHNICIAN')
+            || $this->security->isGranted('ROLE_INVENTORY_MANAGER')
+            || $this->security->isGranted('ROLE_HR_MANAGER');
+    }
+}
