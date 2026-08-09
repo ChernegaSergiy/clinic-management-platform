48980a4723c9d19d44ec779fccb39fa5c3a39c00
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-09 11:55:14 +0300

    refactor: Add basic security roles check to RoomVoter

--- src/Bundles/RoomBundle/RoomVoter.php
+++ src/Bundles/RoomBundle/RoomVoter.php
@@ -24,18 +24,40 @@
 
 namespace App\Bundles\RoomBundle;
 
+use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 class RoomVoter extends Voter
 {
+    public const MANAGE = 'ROOM_MANAGE';
+
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     protected function supports(string $attribute, mixed $subject) : bool
     {
-        return false;
+        return self::MANAGE === $attribute;
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token) : bool
     {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        // Only administrators and medical managers can manage rooms
+        if ($this->security->isGranted('ROLE_ADMIN') || $this->security->isGranted('ROLE_MEDICAL_MANAGER')) {
+            return true;
+        }
+
         return false;
     }
 }
