2398de0214fecfdb797ec1ee229cb68d4d8b328a
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-09 11:59:48 +0300

    refactor: Add basic security roles check to BillingVoter

--- src/Bundles/BillingBundle/BillingVoter.php
+++ src/Bundles/BillingBundle/BillingVoter.php
@@ -2,18 +2,46 @@
 
 namespace App\Bundles\BillingBundle;
 
+use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 class BillingVoter extends Voter
 {
+    public const VIEW = 'BILLING_VIEW';
+    public const MANAGE = 'BILLING_MANAGE';
+
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     protected function supports(string $attribute, mixed $subject) : bool
     {
-        return false;
+        return in_array($attribute, [self::VIEW, self::MANAGE], true);
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token) : bool
     {
+        $user = $token->getUser();
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        // Administrators and Medical Managers have full access to billing
+        if ($this->security->isGranted('ROLE_ADMIN') || $this->security->isGranted('ROLE_MEDICAL_MANAGER')) {
+            return true;
+        }
+
+        switch ($attribute) {
+            case self::VIEW:
+            case self::MANAGE:
+                return $this->security->isGranted('ROLE_REGISTRAR');
+        }
+
         return false;
     }
 }
