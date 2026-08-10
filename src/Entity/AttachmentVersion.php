9e2625a526fd53ba368023098e0fbaf46ee893df
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-10 10:17:07 +0300

    feat: Connect AttachmentVersion entity to repository

--- src/Entity/AttachmentVersion.php
+++ src/Entity/AttachmentVersion.php
@@ -24,10 +24,11 @@
 
 namespace App\Entity;
 
+use App\Core\Repository\AttachmentVersionRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: AttachmentVersionRepository::class)]
 #[ORM\Table(name: 'attachment_versions')]
 class AttachmentVersion
 {
