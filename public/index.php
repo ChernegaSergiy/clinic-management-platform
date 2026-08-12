3605c9faf501c6eed1d522fc22393628a85c7fa0
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-12 19:53:04 +0300

    fix(session): Remove manual session_start() conflicting with Symfony session abstraction

--- public/index.php
+++ public/index.php
@@ -4,10 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-if (PHP_SESSION_NONE === session_status()) {
-    session_start();
-}
-
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
